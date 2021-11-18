12576fb45c25ba0f87a19179f8ad4b468dbd8b57
Luiza Oliveira <luizaisabel2000@gmail.com>
2021-11-18 20:07:00 -0300

    third commit

---
+++ EXIF/EXIF/src/A02EX03.java
@@ -0,0 +1,39 @@
+// IF, ELSE IF, ELSE
+
+// EXERCÍCIO: Construa uma calculadora utilizando a linguagem JAVA que receba 2 valores e execute as operações:
+// soma, subtração, divisão, multiplicação
+// No final deve ser apresentado no console o resultado
+// Caso seja uma operação de divisão, deve ser apresentado também o resto da divisão(mod) 
+
+import java.util.Scanner;
+
+public class A02EX03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        float n1 = scan.nextFloat();
+
+        System.out.print("Digite outro número: ");
+        float n2 = scan.nextFloat();
+
+        System.out.print("Informe a operação (+, -, /, *): ");
+        String operacao = scan.next();
+
+        if(operacao.equals("+")){
+            float soma = (n1 + n2);
+            System.out.print(soma);
+        } else if(operacao.equals ("-")){
+            float sub = (n1 - n2);
+            System.out.print(sub);
+        } else if(operacao.equals ("/")){
+            float div = (n1 / n2);
+            float resto = (n1 % n2);
+            System.out.print("Divisão: " + div + " Resto: " + resto);
+        } else if(operacao.equals ("*")){
+            float multi = (n1 * n2);
+            System.out.print(multi);
+        }
+        scan.close();
+    }
+}
